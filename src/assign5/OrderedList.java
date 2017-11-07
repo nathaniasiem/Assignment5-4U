520ef5a524aeba33880329c319908afb74c528c0
simon7323 <simon7323@HRH-IC0027344.wrdsb.ca>
2017-11-07 10:09:57 -0500

    7/11/2017 Started Q1

--- src/assign5/OrderedList.java
+++ src/assign5/OrderedList.java
@@ -9,59 +9,40 @@ package assign5;
  * @author simon7323
  */
 public class OrderedList {
-    
-    //create variables for the list
+
     private IntNode1 head;
     private int numItems;
 
+    public OrderedList() {
+        head = null;
+        numItems = 0;
+    }
+
+    public boolean isEmpty() {
+        return numItems == 0;
+    }
+
     public void add(int num) {
-     //start at the front of the list
-        IntNode1 start = head;
+        //begins from the top of the list
+        IntNode1 node = head;
         //check to see if there is the first item
-        if (start == null) {
+        //then sets 
+        if (node == null) {
             IntNode1 temp = new IntNode1(num);
             head = temp;
         } else {
             //travel to the end
-            while (start.getNext() != null) {
+            while (node.getNext() != null) {
                 //travel to the next node
-                start = start.getNext();
+                node = node.getNext();
             }
             //node is the last node in the the list
             //create the  new node
             IntNode1 temp = new IntNode1(num);
             //insert it into the list
-            start.setNext(temp);
+            node.setNext(temp);
         }
-        
-    public get(int index){
-        
-        
-    }
-        //increase item counter
-        numItems++;   
-    }
-    
-    public void remove (int num){
-        
-        //decrease item counter
-        numItems--;
-    }
-    
-
-    /**
-     *
-     * @return
-     */
-    public int size() {
-        return numItems;
-    }
-
-    /**
-     *
-     * @return
-     */
-    public boolean isEmpty() {
-        return numItems == 0;
+        //increase the size counter
+        numItems++;
     }
 }
