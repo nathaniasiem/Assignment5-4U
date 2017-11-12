3a2edd4ff424ef10fc713ad94a88d0c8b40695b0
candy <candy@192.168.0.13>
2017-11-12 16:26:44 -0500

    9/12/2017 - q3 initial commit

--- src/assign5/A5Q3.java
+++ src/assign5/A5Q3.java
@@ -9,7 +9,46 @@ package assign5;
  * @author simon7323
  */
 public class A5Q3 {
+    
+    //store the letters for words
+    private char[] letters;
+    
+    //keep track of number of items
+    private int numItems;
 
+    public A5Q3 (){
+        //create constructor to start the array
+        letters= new char[1];
+        numItems=0;
+    }
+        public boolean inLang(String word){
+            //use a for loop to go through each letter
+             for (int i = 0; i < word.length(); i++) {
+            //when there is space to 
+            if (numItems < letters.length) {
+                //increase number of letters in the stack
+                letters[i] = word.charAt(i);
+                numItems++;
+        }
+             //Check if there is a dollar sign in the middle of the word
+        if (letters[(numItems - 1) / 2] == '$') {
+            //go thought hte positions of the array
+            for (int i = 0; word.length >= i; i++) {
+                //Break through the loop if the chars on each side of the word closing in, doesnt equal zero 
+                if (letters[i] != letters[(numItems - 1) - i]) {
+                    
+                    break;
+                }
+                //Return true if the loop doesn't break 
+                return true;
+            }
+        }
+        //Return fals eif the loop does break
+        return false;
+    }
+
+    }
+  
     /**
      * @param args the command line arguments
      */
