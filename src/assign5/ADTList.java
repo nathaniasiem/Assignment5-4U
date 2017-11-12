67c8dff0ed4569f410dddf7b8bb3ed29d40dbb9d
candy <candy@192.168.0.17>
2017-11-12 14:50:18 -0500

    9/12/2017 - initial commit of q2

--- src/assign5/ADTList.java
+++ src/assign5/ADTList.java
@@ -9,7 +9,36 @@ package assign5;
  * @author simon7323
  */
 public class ADTList {
-
+    
+ private int [] List;
+ private int numItems;
+ 
+ public ADTList(){
+    List = new int[10];
+    numItems = 0;
+}
+    public void add(int index,int item){
+        //if there is room in the stack
+        if (numItems<List.length){
+            //put item on the stack
+        List[numItems]=item;
+        numItems++;
+        }else{
+            //make more room
+            int[] temp = new int[List.length*2];
+            //copy items over
+            for(int i=0;i<List.length;i++){
+                temp[i]=List[i];
+            }
+            //stack becomes new array
+            List=temp;
+            //add item
+           List[numItems]=item;
+            numItems++;
+        }
+        
+    }
+ 
     /**
      * @param args the command line arguments
      */
