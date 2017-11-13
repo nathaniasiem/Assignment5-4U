5524acebc90e3be9de034a364597ca7091787112
candy <candy@192.168.0.13>
2017-11-12 19:04:40 -0500

    9/12/2017 -modifications made to q2

--- src/assign5/ADTList.java
+++ src/assign5/ADTList.java
@@ -9,20 +9,21 @@ package assign5;
  * @author simon7323
  */
 public class ADTList {
-    
- private int [] List;
- private int numItems;
- 
- public ADTList(){
-    List = new int[10];
-    numItems = 0;
-}
-     public void add(int index, int num) {
+
+    private int[] List;
+    private int numItems;
+
+    public ADTList() {
+        List = new int[10];
+        numItems = 0;
+    }
+
+    public void add(int index, int num) {
         //at the front of the list 
-        if (index == 0 && num==0) {
-           List[0]=num;
+        if (index == 0 && num == 0) {
+            List[0] = num;
             //adding at the end of the list
-        }  else if (index<numItems){
+        } else if (index < numItems) {
             for (int i = numItems; i > index; i--) {
                 List[i] = List[i - 1];
             }
@@ -42,8 +43,31 @@ public class ADTList {
             List = temp;
         }
     }
-        
-         
+
+    public void remove(int index) {
+        //check if you can remove an item from the list
+        if (index < numItems) {
+            //shuffle the items backward until you reach the index
+            for (int i = index; i <= numItems; i++) {
+                List[i] = List[i + 1];
+            }
+            //decrease the list size
+            numItems--;
+        }
+    }
+
+    public int size() {
+        return numItems;
+    }
+
+    public boolean isEmpty() {
+        return numItems == 0;
+    }
+
+    public int get(int index) {
+        return List[index];
+    }
+
     /**
      * @param args the command line arguments
      */
