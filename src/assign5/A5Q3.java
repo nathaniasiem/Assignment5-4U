83daf86409e0da647743394a958401ecc2f40558
candy <candy@192.168.0.13>
2017-11-12 19:00:01 -0500

    9/12/2017 -modifications made to q3

--- src/assign5/A5Q3.java
+++ src/assign5/A5Q3.java
@@ -9,46 +9,58 @@ package assign5;
  * @author simon7323
  */
 public class A5Q3 {
-    
+
     //store the letters for words
     private char[] letters;
-    
+
     //keep track of number of items
     private int numItems;
 
-    public A5Q3 (){
+    public A5Q3() {
         //create constructor to start the array
-        letters= new char[1];
-        numItems=0;
+        letters = new char[1];
+        numItems = 0;
     }
-        public boolean inLang(String word){
-            //use a for loop to go through each letter
-             for (int i = 0; i < word.length(); i++) {
-            if(numItems<word.length()){
+
+    public boolean inLang(String word) {
+        //use a for loop to go through each letter
+        for (int i = 0; i < word.length(); i++) 
+        {
+            if (numItems < word.length()) {
                 numItems++;
-                letters[i]=word.charAt(i);
-            }else{
-                char[] temp = new char[letters.length*2];
-                for (int j = 0; j < letters.length; j++) {
-                    temp[j] = letters[j];
+                letters[i] = word.charAt(i);
+            } else {
+                char[] temp = new char[letters.length * 2];
+                for (int n = 0; n < letters.length; n++) {
+                    temp[n] = letters[n];
                 }
                 //Make the temp array equal the original array
                 letters = temp;
                 letters[i] = word.charAt(i);
                 numItems++;
             }
+        }
+        //create an if statement if there is a dollar sign in the middle of the word
+        if (letters[(numItems - 1) / 2] == '$') {
+            //go through the positions of the array
+            for (int i = 0; i < letters.length; i++) {
+                //check if the charcters on each side of the word closing in, doesn't equal to zero
+                //then break the loop
+                if (letters[i] != letters[(numItems - 1) - i]) {
+                    break;
+                }
+                //return true when it doesn't meet a condition
+                //indicates that the loop is not broken
+                return true;
             }
-                 
-            }
-                   
-             
-             }
-           
-  
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+        }
+        //Return false when it meets a condition
+        return false;
     }
+    
+public static void main(String[]args){
+    A5Q3 test = new A5Q3();
+     String word = "cat$tac";
+System.out.println("In proper format: " + test.inLang(word));
+}
 }
